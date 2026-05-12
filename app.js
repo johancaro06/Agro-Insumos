f515c90b17f77cd144c2f1ab947c9af096eeac63
johan <johancarovaron123@gmail.com>
2026-05-12 12:24:11 -0500

    Fix Railway PORT

--- app.js
+++ app.js
@@ -28,9 +28,8 @@ app.use((req, res, next) => {
     next();
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log("---------------------------------------------------------");
-    console.log(`🚀 AgroInsumos Backend ejecutándose en http://localhost:${PORT}`);
-    console.log("---------------------------------------------------------");
+    console.log(`🚀 AgroInsumos Backend ejecutándose en puerto ${PORT}`);
 });
